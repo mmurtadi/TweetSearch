a51ca0f7882f32e0f76024c7e08dd7753d4732e1
mmurtadi <mmurtadi@gmail.com>
2016-11-06 23:49:36 -0500

    weekend Twitter API work, need to bootstrapify it this week and publish on my website website

---
+++ constants.py
@@ -0,0 +1,6 @@
+CONSUMER_KEY = 'O8ypOYivKfaMFaCtkRRHwP2Uh'
+CONSUMER_SECRET = 'ge2QuuPGot5ARJsNfeqTMJsaXapePQzXf55iQNSAWYVp8LD8R3'
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
